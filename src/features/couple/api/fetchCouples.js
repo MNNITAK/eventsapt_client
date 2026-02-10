cfc4cce21751dcca1323955c5be8c773c579bb6c
MNNITAK <ay9120991471@gmail.com>
2026-02-10 15:03:28 +0530

    Removed these logic of extracting the stored posts,as we have connected to the microservice endpoint

--- src/features/couple/api/fetchCouples.js
+++ src/features/couple/api/fetchCouples.js
@@ -1,17 +1,20 @@
 import { getCookies } from "@/app/action";
 import { axiosInstance } from "@/axios/axios";
+
+// DISABLED - Will be replaced with microservice logic
 const fetchCouple=async(index,per_page)=>{
     try {
-        let response=await axiosInstance.get(`/cmn/getCouplePosts?searchIndex=${index}&per_page=${per_page}`,{
-            headers:{
-                'wedoraCredentials':await getCookies()
-            }
-        })
-        //console.log(response?.data?.data);
-        response?.data?.data?.cposts?.map((i)=>i.p_type='couple')
-        return response?.data?.data
+        // TODO: Implement with new microservice endpoints
+        // let response=await axiosInstance.get(`/cmn/getCouplePosts?searchIndex=${index}&per_page=${per_page}`,{
+        //     headers:{
+        //         'wedoraCredentials':await getCookies()
+        //     }
+        // })
+        // response?.data?.data?.cposts?.map((i)=>i.p_type='couple')
+        // return response?.data?.data
+        return { cposts: [], totalPages: 0 };
     } catch (error) {
-        
+        return { cposts: [], totalPages: 0 };
     }
 }
 export {fetchCouple}
--- src/features/feed/api/fetchPosts.js
+++ src/features/feed/api/fetchPosts.js
@@ -1,25 +1,34 @@
 import { getCookies } from "../../../app/action.js";
 import { axiosInstance, pseudoAxios } from "../../../axios/axios.js";
+
+// DISABLED - Will be replaced with microservice logic
 export const fetchPosts=async(index,per_page,filter=[])=>{
     try {
-        let resp=await axiosInstance.post(`/cmn/getPosts?searchIndex=${index}&per_page=${per_page}&searchStatus=${filter?.length>0}`,
-           filter,
-            {
-            headers:{
-                'wedoraCredentials':await getCookies()
-            }
-        })
-        resp?.data?.data?.pics.map((i,p)=>i.p_type='post')
-        return resp?.data?.data
+        // TODO: Implement with new microservice endpoints
+        // let resp=await axiosInstance.post(`/cmn/getPosts?searchIndex=${index}&per_page=${per_page}&searchStatus=${filter?.length>0}`,
+        //    filter,
+        //     {
+        //     headers:{
+        //         'wedoraCredentials':await getCookies()
+        //     }
+        // })
+        // resp?.data?.data?.pics.map((i,p)=>i.p_type='post')
+        // return resp?.data?.data
+        return { pics: [], totalPages: 0 };
     } catch (error) {
+        return { pics: [], totalPages: 0 };
     }
 }
+
+// DISABLED - Will be replaced with microservice logic
 export const fetchReels=async(index,per_page)=>{
     try {
-         let resp=await pseudoAxios.get(`/cmn/getReels?searchIndex=${index}&per_page=${per_page}`)
-        resp?.data?.data?.reels.map((i,p)=>i.p_type='reel')
-        //console.log(resp?.data?.data);
-        return resp?.data?.data
-    } catch (error) {        
+        // TODO: Implement with new microservice endpoints
+        // let resp=await pseudoAxios.get(`/cmn/getReels?searchIndex=${index}&per_page=${per_page}`)
+        // resp?.data?.data?.reels.map((i,p)=>i.p_type='reel')
+        // return resp?.data?.data
+        return { reels: [], totalPages: 0 };
+    } catch (error) {
+        return { reels: [], totalPages: 0 };        
     }
 }
