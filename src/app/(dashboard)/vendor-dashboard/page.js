78bd5c7ae2d7c7ea5f542e4152f9da37ab1dbce5
MNNITAK <ay9120991471@gmail.com>
2026-02-10 15:06:04 +0530

    Main export file for vendor dashboard

---
+++ src/app/(dashboard)/vendor-dashboard/page.js
@@ -0,0 +1,10 @@
+'use client'
+import { VendorDashboard } from '@/features/vendor/dashboard'
+
+/**
+ * Vendor Dashboard Page
+ * Complete control panel for vendor management
+ */
+export default function VendorDashboardPage() {
+  return <VendorDashboard />
+}
