4de75c2d99d81bd11ccb7dfd07506663a68845b8
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-03-25 22:47:16 +0530

    added breadcrump navigation

--- src/features/user/dashboard/BreadCrumb.js
+++ src/features/user/dashboard/BreadCrumb.js
@@ -1,5 +1,11 @@
+'use client'
 import { ChevronRight } from "lucide-react";
 import { House } from "lucide-react";
+import { usePathname } from "next/navigation";
+function formatPath(path) {
+  const parts = path.split("/").filter(Boolean);
+  return "/ " + parts.join(" / ");
+}
 export function Separotor() {
     return (
         <span className="ml-2 mr-2">
@@ -7,17 +13,28 @@ export function Separotor() {
         </span>
     );
 }
-export  function Breadcrumb({ items = {} }) {
+export  function Breadcrumb() {
+  const path = usePathname();
+  
+  const parts = path.split("/").filter(Boolean);
   return (
     <div className="flex items-center text-sm bg-white/80 w-[95%] mx-auto backdrop-blur px-6 py-4 rounded-lg justify-between border shadow-sm">
         {/* page description here */}
       <span className="font-semibold text-gray-700">User Profile</span>
       {/* routes link */}
-      <div className="flex items-center">
-       <span className="flex items-center"> <House color="#9a2143" size={18} /><Separotor /></span>
-        <span className="bg-[#f4d8e1] text-[14px] text-[#9a2143] px-2  rounded-md">user-profile</span>
-        
-      </div>
+      <div className="flex items-center gap-2">
+      <span className="flex items-center"><House size={18} color="#9a2143" /><Separotor/></span>
+
+      {parts.map((part, index) => (
+        <div key={index} className="flex items-center gap-2">
+          <span className="bg-[#f4d8e1] text-[14px] text-[#9a2143] px-2 rounded-md">
+            {part}
+          </span>
+
+          {index !== parts.length - 1 && <span>/</span>}
+        </div>
+      ))}
+    </div>
     </div>
   );
 }
