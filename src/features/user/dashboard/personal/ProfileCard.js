1358041e4c80f7e442889c9840a59268d9dc717d
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-03-31 14:06:52 +0530

    personal portfolio design change

--- src/features/user/dashboard/personal/ProfileCard.js
+++ src/features/user/dashboard/personal/ProfileCard.js
@@ -3,14 +3,14 @@
 import Image from "next/image";
 import { Camera } from "lucide-react";
 import { PenLine } from "lucide-react";
-export  function ProfileHeroCard() {
+export function ProfileHeroCard() {
   return (
-    <div className="bg-white w-[95%] mx-auto mt-5 rounded-3xl shadow-md overflow-hidden">
+    <div className="bg-white w-[95%] mx-auto mt-5 rounded-3xl rounded-t-[60px] shadow-md overflow-hidden">
       {/* Cover */}
       <div className="relative h-[20vh] bg-gradient-to-br from-indigo-200 via-purple-200 to-slate-100">
-        <button className="absolute top-4 flex items-center text-[10px] right-4 rounded-md bg-[#C94C73] text-white p-1.5 ">Change Cover <PenLine size={13} className="ml-1" color="#ffffff" /></button>
+        <button className="absolute bottom-4 flex items-center text-[10px] right-4 rounded-md bg-[#C94C73] text-white p-1.5 ">Change Cover <PenLine size={13} className="ml-1" color="#ffffff" /></button>
         {/* Profile Image */}
-        <div className="absolute left-1/2 -bottom-12 -translate-x-1/2">
+        <div className="absolute flex items-center gap-4 top-1/2 -translate-y-1/2 left-5">
           <div className="relative w-28 h-28 rounded-full border-4 border-white shadow-lg overflow-hidden">
             <Image
               src="https://images.pexels.com/photos/2095597/pexels-photo-2095597.jpeg"
@@ -23,15 +23,19 @@ export  function ProfileHeroCard() {
               <Camera size={14} />
             </button>
           </div>
+          <div>
+            <h2 className="text-xl font-semibold">Akshay Yadav</h2>
+            <p className="text-gray-500 text-sm">Organizer</p>
+          </div>
         </div>
       </div>
 
       {/* Content */}
       <div className="pb-4 pt-4  px-6 flex justify-between items-center">
-        <div>
+        {/* <div>
           <h2 className="text-xl font-semibold">Mai kon?</h2>
         <p className="text-gray-500 text-sm">Organizer</p>
-        </div>
+        </div> */}
 
         {/* Stats */}
         <div className="flex justify-center gap-8 ">
