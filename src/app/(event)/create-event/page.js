2691e9ba2e328cb9aa0d75f311bb6f335bea999e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-04-04 16:20:39 +0530

    metadata at create event page

--- src/app/(event)/create-event/page.js
+++ src/app/(event)/create-event/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'Create your Event',
+  description: 'Create yoir event here.',
+}
 import { CreateEventLandingPage } from "@/features/event/event-landing-page/components";
 
 export default function CreateEventPage() {
